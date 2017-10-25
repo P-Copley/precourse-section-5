9bd68def7d029846cb24ff89ff396d9aa00b4421
Paul Copley <pkcopley@gmail.com>
2017-10-25 17:35:34 +0100

    Add css and lose screen/function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var used=[], it=["qwerty","foobar"];
-var word =it[0].split("");
+var used=[], it=["Qqwerty","foobar"], capitals =["London","Paris"];
+var word =it[0].toLowerCase().split("");
 var count =0;
-
 $(document).ready(() => {
+
   function alphabet(){
     for(var i=97; i<123; i++){
       $("#letters").append("<td id="+String.fromCharCode(i)+">"+String.fromCharCode(i)+"</td>")
@@ -18,21 +18,36 @@ $(document).ready(() => {
   alphabet();
 // keypress adds the presses key to used and if id matches reveals correct letters
   $(document).keypress(function() {
-    var guess =String.fromCharCode(event.which);
+    var guess =String.fromCharCode(event.which).toLowerCase();
     if (used.indexOf(guess) === -1){
       $("#guess").append(guess);
       if (word.indexOf(guess) === -1 && used.indexOf(guess) === -1){
-        count ++;
-        $(".gallows img").attr("src","images/"+count+".jpg")
+        if (count <6){
+          count ++;
+          $(".gallows img").attr("src","images/"+count+".jpg")
+        }
       }
       used.push(guess);
     };
     $("#"+guess+"").css("color","red");
-    $("#a"+guess+"").html("<u>"+guess+"</u>")
+    $("[id=a"+guess+"]").html("<u>"+guess+"</u>")
+    end();
   })
+
+  function end(){
+    if (count === 6){
+      document.getElementById("lose").style.display = "block";
+      $("#lose").animate({opacity:"1",height:"100px"},"slow");
+    }
+  }
 });
 
-
+function reveal(){
+  for (var i=0; i<word.length; i++){
+    $("[id=a"+word[i]+"]").html("<u>"+word[i]+"</u>")
+  }
+}
+// Account for caps and space
 
 /*
 variables: list of letters, chosen letters, remaining letters, hangman image + end condition,
